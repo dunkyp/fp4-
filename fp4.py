38deff87cdd50e76711fac084060a33932f89998
Duncan Paterson <dunkyp@gmail.com>
2015-03-01 00:44:37 +0000

    removed debugging setting

--- fp4.py
+++ fp4.py
@@ -27,4 +27,4 @@ def calcualte():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0",debug=True, port=port)
+    app.run(host="0.0.0.0", port=port)
