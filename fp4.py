a70fc1db9f937a4d4061299452058e1e12f26c4b
Duncan Paterson <dunkyp@gmail.com>
2015-04-15 20:44:15 +0100

    added portra

--- fp4.py
+++ fp4.py
@@ -1,22 +1,31 @@
-import os,json
+import os,json, math
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def compensation(x):
+def ilford(x):
     if x <= 0.5:
         return x
     return 66.86823338 * 1.04081939 ** x - 67.19686994
 
+def portra(time):
+    return time + (0.5167 * math.log(time) - 0.2006) * time
+
 @app.route("/")
 def calcualte():
     time = request.args.get('time', None)
+    stock = request.args.get('stock', 'fp4')
     if time:
         try:
             time = float(time)
+            adjusted_time = time
+            if stock == 'fp4' or stock == 'hp5':
+                adjusted_time = ilford(time)
+            elif stock == 'portra':
+                adjusted_time = portra(time)
             return render_template('index.html',
                                    before=time,
-                                   time=compensation(time),
+                                   time=adjusted_time,
                                    shown="visible")
         except:
             return "couldn't understand your time, try again in seconds"
